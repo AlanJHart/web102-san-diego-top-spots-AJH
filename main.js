d846bbeb778bf6cee2e4287a030dd65075f7e8c5
AlanJHart <hart.alan.j@gmail.com>
2019-04-05 01:16:04 -0700

    Passes all tests. Adds initial project files

--- main.js
+++ main.js
@@ -1,4 +1,39 @@
-$(document).ready(function() {
-// write your code here
+// $(document).ready(function() {
+// // write your code here
 
-});
+var data = [];
+$.getJSON("data.json", function(data) {
+// Create empty table (id="data-list" must exist in html)
+var columnHeadings = Object.keys(data[0]);
+var columnCount = columnHeadings.length;
+var rowCount = data.length;
+var table = document.createElement('table');
+document.getElementById("data-list").appendChild(table);
+// Create table header row (thead)
+var header = table.createTHead();
+var row = header.insertRow(-1);
+for (var i = 0; i < columnCount; i++) {
+    var headerCell = document.createElement('th');
+    headerCell.innerText = columnHeadings[i].toUpperCase();
+    row.appendChild(headerCell);
+}
+// Create table body element (tbody)
+var tBody = document.createElement('tbody');
+table.appendChild(tBody);
+
+for(var i = 0; i < data.length; i++) {
+    var nameData = data[i].name;
+    var descrData = data[i].description;
+    var localLongData = data[i].location[0];
+    var localLatData = data[i].location[1];
+    row = table.insertRow(i);
+    var name = row.insertCell(0);
+    var description = row.insertCell(1);
+    var location = row.insertCell(2);
+    name.innerHTML = nameData;
+    description.innerHTML = descrData;
+    location.innerHTML = ("https://www.google.com/maps?q=" + localLongData + "," + localLatData);
+
+}
+})
+
